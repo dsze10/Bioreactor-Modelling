8d9a2332c6f49029796ca50951f89cff2be4abcf
dsze10 <141266552+dsze10@users.noreply.github.com>
2023-08-06 16:44:53 -0400

    Add files via upload

---
+++ NanEmp.m
@@ -0,0 +1,11 @@
+function output = NanEmp(values)
+    mask = isnan(values);
+    if nnz(mask)
+      output = string(values);
+      output(mask) = "";
+      output = char(output);
+    else
+        output = values;
+    end
+end
+
